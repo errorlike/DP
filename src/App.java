94b8d48b723937835009383cd5653a946c8e5658
errorlike <errorlikeman@gmail.com>
2023-09-14 01:36:28 +0800

    记录最便宜的路径

--- src/App.java
+++ src/App.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 public class App {
     /*
      * n = 3 , k = 2
@@ -16,20 +22,35 @@ public class App {
      * f(n)
      */
     public static void main(String[] args) throws Exception {
-        System.out.println(climbStairsWithPrice(0, new int[] { 3, 2, 4 }));
-        System.out.println(climbStairsWithPrice(1, new int[] { 3, 2, 4 }));
-        System.out.println(climbStairsWithPrice(3, new int[] { 3, 2, 4 }));
+        int[] record =   climbStairsWithPrice(8, new int[] { 0, 3, 2, 4, 6, 1, 1, 5, 3 });
+        System.out.println(getBestPath(record));
     }
 
-    public static int climbStairsWithPrice(int n, int[] price) {
+    public static int[] climbStairsWithPrice(int n, int[] price) {
+        int[] path = new int[n + 1];
         int[] cache = new int[n + 1];
         cache[0] = 0;
         if (n >= 1) {
             cache[1] = 3;
             for (int i = 2; i < n + 1; i++) {
-                cache[i] = price[i - 1] + Math.min(cache[i - 1], cache[i - 2]);
+                cache[i] = price[i] + Math.min(cache[i - 1], cache[i - 2]);
+                if (cache[i - 1] < cache[i - 2]) {
+                    path[i] = i - 1;
+                } else {
+                    path[i] = i - 2;
+                }
             }
         }
-        return cache[n];
+        return path;
+    }
+
+    public static List<Integer> getBestPath(int[] record) {
+        List<Integer> path = new LinkedList<>();
+        for (int i = record.length - 1; i > 0; i = record[i]) {
+            path.add(i);
+        }
+        path.add(0);
+        Collections.reverse(path);
+        return path;
     }
 }
