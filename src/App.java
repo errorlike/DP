3f95f1cb9754411e8ab305eb9006d3e5f31b2fe4
errorlike <errorlikeman@gmail.com>
2023-09-16 20:46:46 +0800

    在矩阵中添加障碍物

--- src/App.java
+++ src/App.java
@@ -1,14 +1,25 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int result = toXY(3, 4);
+        int[][] obstacle = new int[][] {
+                new int[] { 0, 0, 0, 0 },
+                new int[] { 0, 0, 1, 1 },
+                new int[] { 0, 0, 0, 0 },
+        };
+        int result = toXY(obstacle);
         System.out.println(result);
     }
 
-    public static int toXY(int m, int n) {
+    public static int toXY(int obstacle[][]) {
+        int m = obstacle.length;
+        int n = obstacle[0].length;
         int[][] cache = new int[m][n];
         cache[0][0] = 1;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
+                if (obstacle[i][j] == 1) {
+                    obstacle[i][j] = 0;
+                    continue;
+                }
                 if (i == 0 || j == 0) {
                     cache[i][j] = 1;
                     continue;
