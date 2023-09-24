b0890393f076e9e0698501a5bb7aa26b8aa77394
errorlike <errorlikeman@gmail.com>
2023-09-25 00:09:46 +0800

    获得最佳路径

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) throws Exception {
         int[][] obstacle = new int[][] {
@@ -10,13 +12,13 @@ public class App {
                 { 3, 1, 1, 100 },
                 { 4, 4, 2, 0 }
         };
-        int result = toXY(obstacle);
-        int result1 = toXY(obstacle1);
+        ArrayList<ArrayList<Integer>> result = toXY(obstacle);
+        ArrayList<ArrayList<Integer>> result1 = toXY(obstacle1);
         System.out.println(result);
         System.out.println(result1);
     }
 
-    public static int toXY(int obstacle[][]) {
+    public static ArrayList<ArrayList<Integer>> toXY(int obstacle[][]) {
         int m = obstacle.length;
         int n = obstacle[0].length;
         int[][] cache = new int[m][n];
@@ -37,6 +39,36 @@ public class App {
                 cache[i][j] = obstacle[i][j] + Math.max(cache[i - 1][j], cache[i][j - 1]);
             }
         }
-        return cache[m - 1][n - 1];
+        return getPath(cache, m - 1, n - 1, new ArrayList<ArrayList<Integer>>());
+    }
+
+    public static ArrayList<ArrayList<Integer>> getPath(int[][] cache, int targetRow, int targetCol,
+            ArrayList<ArrayList<Integer>> path) {
+        if (targetRow == 0 && targetCol == 0) {
+            ArrayList<Integer> coordinate = new ArrayList<>();
+            coordinate.add(targetRow);
+            coordinate.add(targetCol);
+            path.add(coordinate);
+            return path;
+        } else if (targetRow == 0) {
+            path = getPath(cache, targetRow, targetCol - 1, path);
+
+        } else if (targetCol == 0) {
+            path = getPath(cache, targetRow - 1, targetCol, path);
+
+        } else {
+            if (cache[targetRow][targetCol - 1] > cache[targetRow - 1][targetCol]) {
+                path = getPath(cache, targetRow, targetCol - 1, path);
+            } else {
+                path = getPath(cache, targetRow-1, targetCol, path);
+
+            }
+
+        }
+        ArrayList<Integer> coordinate = new ArrayList<>();
+        coordinate.add(targetRow );
+        coordinate.add(targetCol);
+        path.add( coordinate);
+        return path;
     }
 }
