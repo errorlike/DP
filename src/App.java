c7fb55693b82573266a1ce24f78b2cc79ce9d052
errorlike <errorlikeman@gmail.com>
2023-09-29 00:12:28 +0800

    涂栅栏

--- src/App.java
+++ src/App.java
@@ -1,36 +1,26 @@
 public class App {
-    /*
-     * 1. 目标函数：
-     * f(n)
-     * 2.确定基本情况（边界条件）
-     * f(0) = 1
-     * f(1) = 1
-     * 3.递推关系
-     * 3.f(n) = f(n-1)+f(n-2)
-     * 4.由子问题的推导顺序。
-     * 自底至上
-     * 5.定位答案
-     * f(n)
-     */
-    public static void main(String[] args) throws Exception {
-        System.out.println(climbStairs(0));
-        System.out.println(climbStairs(1));
-        System.out.println(climbStairs(3));
-        System.out.println(climbStairs(5));
-        System.out.println(climbStairs(6));
-        System.out.println(climbStairs(10));
-
+    // f(i,j) = f(i-1,1-j)+f(i-2,1-j)
+    // f(n,0)+f(n,1);
+    // 0 1 00 10 01 11
+    // 建模方式变成：i个fences的最后一个颜色为j时有多少种涂法,子问题为 i-1个fences的颜色，末尾颜色为1-j的时的涂法 +
+    // i-2个fence末尾颜色为1-j的时候（此时最后两个同色）
+    public static void main(String[] args) {
+        System.out.println(painted(3));
+        System.out.println(painted(4));
+        System.out.println(painted(5));
     }
 
-    public static int climbStairs(int n) {
-        int[] cache = new int[n + 1];
-        cache[0] = 1;
-        if (n >= 1) {
-            cache[1] = 1;
-            for (int i = 2; i < n + 1; i++) {
-                cache[i] = cache[i - 1] + cache[i - 2];
+    public static int painted(int n) {
+        int[][] cache = new int[n + 1][2];
+        cache[1][0] = 1;
+        cache[1][1] = 1;
+        cache[2][0] = 2;
+        cache[2][1] = 2;
+        for (int i = 3; i <= n; i++) {
+            for (int j = 0; j < 2; j++) {
+                cache[i][j] = cache[i - 1][1 - j] + cache[i - 2][1 - j];
             }
         }
-        return cache[n];
+        return cache[n][0] + cache[n][1];
     }
 }
