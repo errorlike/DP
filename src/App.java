ab3286d1fc341c1e64f5a77a9fd293d5ffe6470f
errorlike <errorlikeman@gmail.com>
2023-10-13 00:42:31 +0800

    寻找最少的找零硬币数。

--- src/App.java
+++ src/App.java
@@ -2,28 +2,26 @@ public class App {
 
     public static void main(String[] args) {
 
-        System.out.println(makeChange(8, new int[] { 1, 2, 3, 5 }));
-        System.out.println(makeChange(75, new int[] { 1, 2, 3, 5 }));
+        System.out.println(makeChange(2, new int[] { 1, 2, 3, 5 }));
+        System.out.println(makeChange(3, new int[] { 1, 2, 3, 5 }));
+        System.out.println(makeChange(4, new int[] { 1, 2, 3, 5 }));
+        System.out.println(makeChange(29, new int[] { 1, 3, 5 }));
+        System.out.println(makeChange(1, new int[] { 2, 3, 5 }));
+        System.out.println(makeChange(56, new int[] { 15, 4, 3 }));
     }
 
     public static int makeChange(int n, int[] coins) {
-        int column = coins.length;
-        int[][] cache = new int[n + 1][column];
-        for (int i = 0; i < column; i++) {
-            cache[0][i] = 1;
-        }
-        for (int i = 1; i < n + 1; i++) {
-            for (int j = 0; j < column; j++) {
-                for (int k = 0; k <= j; k++) {
-                    if (i < coins[k]) {
-                        // f(3,3) = f(3,5)
-                        // 如果coins是升序的话可以使用break，如果乱序就使用continue.
-                        break;
-                    }
-                    cache[i][j] += cache[i - coins[k]][k];
+        int[] cache = new int[n + 1];
+        cache[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            cache[i] = Integer.MAX_VALUE;
+            for (int j = 0; j < coins.length; j++) {
+                if (i < coins[j] || cache[i - coins[j]] == Integer.MAX_VALUE) {
+                    continue;
                 }
+                cache[i] = Math.min(cache[i], 1 + cache[i - coins[j]]);
             }
         }
-        return cache[n][column - 1];
+        return cache[n];
     }
 }
